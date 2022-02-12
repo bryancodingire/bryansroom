e19da20cf8838e1e94066b592b437044af547dfe
Bryan <laurenkelley@Laurens-MacBook-Pro-69.local>
2022-02-12 16:40:43 +0000

    updated github pages url

--- src/components/BryansRoom.js
+++ src/components/BryansRoom.js
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 function BryansRoom() {
   return (
     <div>
-
       <h1>
         Bryan's Room
       </h1>
 
       <p>Map</p>
-      
+
       <button><Link to="/checkitem">Check Item</Link></button>
 
     </div>
